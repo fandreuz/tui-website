c696d1add58266296733585621dc5ae2750e5168
Alex King <alexanderddking@gmail.com>
2017-09-04 06:03:37 +0900

    WIP - split themes in fetch

--- src/store/reducers/fetch.js
+++ src/store/reducers/fetch.js
@@ -3,8 +3,25 @@ const { fetchThemes } = require('store/firebase')
 function getThemes(state, emitter) {
   fetchThemes((data) => {
     state.themes = data
+    const pages = []
+    let themeCount = 0
+    let pageCount = 0
+    let singlePage = []
+    let globalCount = 0
     Object.keys(data).map((theme) => {
-      // add pagination
+     // add pagination
+      singlePage.push(theme)
+      globalCount++
+      if (singlePage.length === 20) {
+        pages.push(singlePage)
+        singlePage = []
+      }
+      if (singlePage.length > 0 && globalCount === Object.keys(data).length) {
+        pages.push(singlePage)
+        singlePage = []
+      }
+      console.log(pages)
+      // console.log(singlePage)
     })
     state.themesLoaded = true
     emitter.emit('render')
